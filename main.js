85775c33d2bdb2484e93396c724eafccda623f55
Nadavgo8 <19nadav99@gmail.com>
2025-08-01 17:42:32 +0300

    step 4 finalized

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
 import Tweeter from "./model.js";
+import Renderer from "./render.js";
+const renderer = Renderer();
 const tweeter = Tweeter();
 
-// Add a new post
-tweeter.addPost("This is my own post!");
-console.log(tweeter.getPosts());
-
-// Remove a post
-tweeter.removePost("p1");
-console.log(tweeter.getPosts());
-
-// Add comments
-tweeter.addComment("p3", "Damn straight it is!");
-tweeter.addComment("p2", "Second the best!");
-console.log(tweeter.getPosts());
-
-// Remove a comment
-tweeter.removeComment("p2", "c6");
-console.log(tweeter.getPosts());
-
-import Renderer from "./render.js";
-
-const renderer = Renderer();
-
-// This should render the initial dummy data
+// Initial render
 renderer.renderPosts(tweeter.getPosts());
+
+// 1. Add Post (Twit button)
+$("#twit-btn").on("click", function () {
+  const input = $("#input").val();
+  if (!input.trim()) return;
+
+  tweeter.addPost(input);
+  $("#input").val(""); // clear input
+  renderer.renderPosts(tweeter.getPosts());
+});
+
+// 2. Delete Post
+$("#posts").on("click", ".delete", function () {
+  const postId = $(this).data("id");
+  tweeter.removePost(postId);
+  renderer.renderPosts(tweeter.getPosts());
+});
+
+// 3. Add Comment
+$("#posts").on("click", ".comment-button", function () {
+  const $post = $(this).closest(".post");
+  const postId = $post.data("id");
+  const $input = $post.find(".comment-input");
+  const text = $input.val();
+  if (!text.trim()) return;
+
+  tweeter.addComment(postId, text);
+  $input.val("");
+  renderer.renderPosts(tweeter.getPosts());
+});
+
+// 4. Delete Comment (x)
+$("#posts").on("click", ".delete-comment", function () {
+  const commentId = $(this).data("id");
+  const postId = $(this).closest(".post").data("id");
+
+  tweeter.removeComment(postId, commentId);
+  renderer.renderPosts(tweeter.getPosts());
+});
